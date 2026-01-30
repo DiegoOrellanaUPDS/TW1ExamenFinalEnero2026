d41130908c07f48288721f29f62aff8a7c2bc5d9
xfox <jbrayaa789@gmail.com>
2026-01-30 08:53:35 -0400

    Agregada entidad Brayan y controlador POST

---
+++ Controllers/BrayanController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Data;   // <--- REVISA QUE COINCIDA CON TU PROYECTO
+using Entidades; // <--- REVISA QUE COINCIDA CON TU PROYECTO
+
+namespace NombreDelProyecto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrayanController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BrayanController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/Brayan
+        [HttpPost]
+        public async Task<ActionResult<Brayan>> PostBrayan(Brayan brayan)
+        {
+            _context.Brayans.Add(brayan);
+            await _context.SaveChangesAsync();
+            
+            // Retorna 201 Created y el objeto
+            return CreatedAtAction(nameof(PostBrayan), new { id = brayan.Id }, brayan);
+        }
+    }
+}
--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Entidades;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data
 {
@@ -9,6 +10,8 @@ namespace Data
 
         }
         //public DbSet<Docente> Docentes { get; set; }
+
+        public DbSet<Brayan> Brayans { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
